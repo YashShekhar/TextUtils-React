e5ddd09aabbc5472d487b9de691869c7b5c94aea
Yash Shekhar <yashshekhar28@gmail.com>
2023-10-23 18:18:13 +0530

    Fixed bugs and added multiple color themes

--- src/App.js
+++ src/App.js
@@ -25,17 +25,36 @@ function App() {
     }, 1500);
   };
 
-  const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = 'grey';
-      showAlert("Dark mode has been enabled", "success");
-      document.title = 'TextUtils - Dark Mode'
-    } else {
+  const removeBodyClassList = () => {
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-primary');
+  }
+  const toggleMode = (cls) => {
+    removeBodyClassList();
+    document.body.classList.add('bg-' + cls);
+    if (cls === 'success') {
+      setMode('success');
+      showAlert("Success mode has been enabled", "success");
+    } else if (cls === 'warning') {
+      setMode('warning');
+      showAlert("Warning mode has been enabled", "success");
+    } else if (cls === 'danger') {
+      setMode('danger');
+      showAlert("Danger mode has been enabled", "success");
+      // document.title = 'TextUtils - Dark Mode'
+    } else if (cls === 'primary') {
+      setMode('primary');
+      showAlert("Primary mode has been enabled", "success");
+    } else if (cls === 'light') {
       setMode('light');
-      document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = 'TextUtils - Light Mode'
+    } else {
+      setMode('dark');
+      showAlert("Dark mode has been enabled", "success");
     }
   };
   return (
@@ -45,8 +64,8 @@ function App() {
         <Alert alert={alert} />
         <div className="container my-3">
           <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text below to analyze" mode={mode} />} />
+            <Route exact path="/about" element={<About mode={mode} />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
           </Routes>
         </div>
       </Router>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,7 +4,6 @@ import React, { useState } from 'react'
 export default function TextForm(props) {
     const [text, setText] = useState("Enter text here");
     const handleUpperCaseClick = () => {
-        // console.log("Uppercase clicked1!!");
         let newText = text.toUpperCase();
         setText(newText);
         props.showAlert("Converted to Upper Case!!", "success");
@@ -20,9 +19,7 @@ export default function TextForm(props) {
         props.showAlert("Text Cleared!!", "success");
     };
     const handleCopy = () => {
-        let text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied Text!!", "success");
     };
     const handleExtraSpace = () => {
@@ -31,28 +28,40 @@ export default function TextForm(props) {
         props.showAlert("Removed Extra Space!!", "success");
     };
     const handleOnChange = (event) => {
-        // console.log("On Change clicked1!!");
         setText(event.target.value);
+    };
+
+    const getModeStyle = (cls) => {
+        if (cls === 'dark') {
+            return {
+                backgroundColor: '#b5b5b5',
+                color: 'white'
+            }
+        } else {
+            return {
+                color: 'black'
+            }
+        }
     };
     return (
         <>
-            <div className='container' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
-                <h1>{props.heading}</h1>
+            <div className='container'>
+                <h1 className='mb-4' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8" style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}></textarea>
+                    <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8" style={getModeStyle(props.mode)}></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpperCaseClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLowerCaseClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>
-                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleUpperCaseClick}>Convert to Uppercase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleLowerCaseClick}>Convert to Lowercase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear</button>
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p><i>You can read above text in {0.008 * text.split(" ").length} Minutes</i></p>
+                <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+                <p><i>You can read above text in {0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Minutes</i></p>
                 <h2>Preview</h2>
-                <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
+                <p>{text.length > 0 ? text : "Nothing to Preview!!"}</p>
             </div>
         </>
     );
